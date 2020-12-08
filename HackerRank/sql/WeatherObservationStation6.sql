dec35ca3f01624bccd749f816d702e2856b0e948
Saon00 <57843701+Saon00@users.noreply.github.com>
2020-12-08 22:03:07 +0600

    resources added

--- HackerRank/sql/WeatherObservationStation6.sql
+++ HackerRank/sql/WeatherObservationStation6.sql
@@ -3,3 +3,8 @@ answer credit: @eternal
 */
 
 SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*";
+
+/* Some references:
+https://stackoverflow.com/questions/36627613/sql-query-to-check-if-a-name-begins-and-ends-with-a-vowel
+https://www.sqlshack.com/t-sql-regex-commands-in-sql-server/
+*/
