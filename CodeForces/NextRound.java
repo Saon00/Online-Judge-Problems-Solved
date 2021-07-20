3b7a7dcc5c2bd37fdddc123d1a8e1099e4f6dd52
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-07-20 21:25:15 +0600

    158A

---
+++ CodeForces/NextRound.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt(), k = scan.nextInt();
+        int count = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+            if (arr[i] >= arr[k - 1] && arr[i] != 0)
+                count++;
+        }
+
+
+        System.out.println(count);
+
+
+    }
+}
