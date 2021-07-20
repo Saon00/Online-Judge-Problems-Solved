7aa51be3bccb789193170d03b906aba82b57ae57
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-07-20 15:05:47 +0600

    1A

---
+++ CodeForces/TheatreSquare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long n = scan.nextLong(), m = scan.nextLong(), a = scan.nextLong();
+        long result = n % a == 0 ? n / a : n / a + 1;
+        result = result * (m % a == 0 ? m / a : m / a + 1);
+        System.out.println(result);
+        
+    }
+}
