d1fed74fdd9324d5bc33bf40e9c1cf7511c6709b
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-07-20 22:02:27 +0600

    50A

---
+++ CodeForces/DominoPiling.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int m = scan.nextInt(), n = scan.nextInt();
+        System.out.println(m*n/2);
+        
+    }
+}
+
+// https://codeforces.com/problemset/problem/50/A
