f1f540c4e8c08e7ca1d01db8ea726ab7ff4a341b
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-07-26 10:54:23 +0600

    977A

---
+++ CodeForces/WrongSubtraction.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(), k = scan.nextInt();
+        
+        while (k-- > 0) {
+            n = lastNumber(n) == 0 ? n / 10 : n - 1;
+        }
+        System.out.println(n);
+
+    }
+
+    public static int lastNumber(int n) {
+        int last = n % 10;
+        return last;
+    }
+}
+
+// https://codeforces.com/problemset/problem/977/A
