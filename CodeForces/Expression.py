0565728b6e1ed11f54c06a1a450f0a7f24f8885d
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-08-02 18:42:41 +0600

    479A

---
+++ CodeForces/Expression.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+l = [(a + (b * c)), (a * (b + c)), (a * b * c), (a + b) * c, (a + b + c)]
+print(max(l))
+
+# https://codeforces.com/problemset/problem/479/A
